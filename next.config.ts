516bcea578e6837f9cd5ba545ac4e5c82167fd3c
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2025-02-26 18:41:31 +0400

    lemn project update next config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: '/lemn',
-  assetPrefix: '/lemn/',
-  exportTrailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
